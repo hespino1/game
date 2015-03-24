1c5718c26cf8bfcb4ee62daeee947649c43454e0
Hugo E <hespino1@eafit.edu.co>
2015-03-24 11:40:05 -0500

    Correcion validacion active

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,7 @@ class Player < ActiveRecord::Base
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	validates :username, :presence => {:message => "can't be empty."}, uniqueness: true, length: {minimum: 5, maximum: 20}
 	validates :points, :numericality => { :greater_than_or_equal_to => 0 }
-
+	validates :active, :presence => {:message => "can't be empty."}
 
 	# => validates :name, format: { with: /\A[a-zA-Z]+\z/,
     # => message: "only allows letters" }
